2191dc6eac60c05d09a2fa3b30bdaa10a05e2684
amarimancillas <75327415+amarimancillas@users.noreply.github.com>
2024-02-12 22:56:11 -0600

    Update modulefive.cpp

--- modulefive.cpp
+++ modulefive.cpp
@@ -3,15 +3,26 @@
 
 using namespace std;
 
-int main(){
-    string name;
+int main() {
+    string fullName;
     
-    cout << "Please type your full name: ";
-    //cin >> name;
+    cout << "Enter your full name: ";
+    getline(cin, fullName);
+    
+    size_t spacePos = fullName.find(' ');
 
-    getline(cin, name);
+    string firstName = fullName.substr(0, spacePos);
+    string lastName = fullName.substr(spacePos + 1);
+    
+    cout << "Initials: " << firstName[0] << "." << lastName[0] << "." << endl;
 
-    cout << name << endl;
+    string newFirstName = firstName.substr(0, firstName.length() / 2) +
+                          lastName.substr(lastName.length() / 2);
+    string newLastName = lastName.substr(0, lastName.length() / 2) +
+                         firstName.substr(firstName.length() / 2);
 
+    cout << "New First Name: " << newFirstName << endl;
+    cout << "New Last Name: " << newLastName << endl;
+    
     return 0;
 }
