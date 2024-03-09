e5db047142570d758301281137ba9c56c9579877
amarimancillas <75327415+amarimancillas@users.noreply.github.com>
2024-03-09 16:00:47 +0000

    yes!

---
+++ module10.1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int factorial(int n) {
+    int result = 1;
+    
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+
+    return result;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // Calculate factorial using the factorial function
+    int result = factorial(num);
+
+    cout << "Factorial of " << num << " is: " << result << endl;
+
+    return 0;
+}
