a238e845374f977fa52c185b2a94d294c08536db
amarimancillas <75327415+amarimancillas@users.noreply.github.com>
2024-05-08 16:04:49 +0000

    yes!

--- module13.cpp
+++ module13.cpp
@@ -2,7 +2,35 @@
 
 using namespace std;
 
+class Dog{
+public:
+    void bark(){
+        cout << "woof woof!" << endl;
+    }
+};
+
+class waterBottle{
+public:
+    void spill(){
+        cout << "*spills out of bottle*" << endl;
+    }
+};
+
+class computer{
+public:
+    void overheats(){
+        cout << "*overheats and catches on fire*" << endl;
+    }
+};
+
 int main(){
+    computer pc;
+    waterBottle water;
+    Dog wolf;
+
+    wolf.bark();
+    water.spill();
+    pc.overheats();
 
     return 0;
 }
